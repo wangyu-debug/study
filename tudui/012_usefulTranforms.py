3ef7a74ad27230f31fb27216fcb91573867717c2
wangyu-debug <70136801+wangyu-debug@users.noreply.github.com>
2023-11-07 15:58:05 +0800

    transform的使用

--- tudui/012_usefulTranforms.py
+++ tudui/012_usefulTranforms.py
@@ -28,6 +28,26 @@ print(img_resize)
 img_resize  = trans_totensor(img_resize)
 writer.add_image("img_resize",img_resize,0)
 
+#Compose - resize-2
+trans_resize_2 = transforms.Resize(512)
+trans_compose = transforms.Compose([trans_resize_2,trans_totensor])
+img_resize_2 = trans_compose(img)
+print(img_resize_2[0][0][0])
+writer.add_image("Resize",img_resize,1)
+
+#RandomCrop
+trans_random = transforms.RandomCrop((200,222))
+trans_compose2  = transforms.Compose([trans_random,trans_totensor])
+for i in range(10):
+    img_crop = trans_compose2(img)
+    writer.add_image("RandomCrop",img_crop,i)
+
+print(trans_compose2(img)[0][0][0])
+
+
+
+
+
 
 writer.close()
 
